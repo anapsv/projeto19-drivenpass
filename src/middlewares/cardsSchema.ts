4b0c78e14ed940f159d4394e2d1ba2fb23752cfd
ana <anapsilva793@gmail.com>
2022-09-12 16:58:52 -0300

    thunderclient reqs

--- src/middlewares/cardsSchema.ts
+++ src/middlewares/cardsSchema.ts
@@ -3,7 +3,7 @@ import joi from 'joi';
 const cardSchema = joi.object({
     title: joi.string().required(),
     number: joi.string().length(16).pattern(/^[0-9]+$/).required(),
-    cardHolderName: joi.string().required(),
+    cardholderName: joi.string().required(),
     securityCode: joi.string().length(3).pattern(/^[0-9]+$/).required(),
     expirationDate: joi.string().pattern(/^(0[1-9]|1[012])[-\/.]([0-9][0-9])$/).required(),
     password: joi.string().pattern(/^[0-9]+$/).required(),
